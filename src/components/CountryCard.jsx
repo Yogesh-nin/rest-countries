7a8ad34f08151bdd8f10059cf793a9a671b98deb
Yogesh-nin <002chandra.yogesh@gmail.com>
2022-03-18 00:16:49 +0530

    fetching api

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import './CountryCard.css'
+const CountryCard = (props) => {
 
-const CountryCard = () => {
+  const style = {
+    backgroundColor: props.mode === 'light' ? 'hsl(0, 0%, 100%)' :'hsl(209, 23%, 22%)',
+    color: props.mode === 'light' ? 'hsl(200, 15%, 8%)' : 'hsl(0, 0%, 100%)',
+
+    height: '295px'
+  }
   return (
     <div>
-        <div class="card">
-            <img src="..." class="card-img-top" alt="..." />
+        <div class="card" style={style} >
+            <img src={props.flag} class="card-img-top" alt="..." />
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <h5 class="card-title">{props.title}</h5>
+                <span className='card-content'><strong>Population: </strong>{props.population}</span> <br></br>
+                <span className='card-content'><strong>Region: </strong> {props.region}</span><br></br>
+                <span className='card-content'><strong>Capital:</strong>  {props.capital}</span><br></br>
             </div>
             </div>
     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,20 @@
 import React, {useState, useEffect} from 'react'
+import axios from './axios/index'
 import CountryCard from './CountryCard'
 import Header from './Header'
 const Home = (props) => {
+    const [countriesData, setCountriesData] = useState([])
 
     const style = {
         backgroundColor: props.mode === 'light' ? 'hsl(0, 0%, 100%)' :'hsl(207, 26%, 17%)',
         color: props.mode === 'light' ? 'hsl(200, 15%, 8%)' : 'hsl(0, 0%, 100%)'
     }
 
-    const [countries, setCountries] = useState([]);
-
     const updateCountries = async () => {
-        const url = 'https://restcountries.com/v2/all'
-        let data = await fetch(url);
-        let parsedData = await data.json()
-
+        
+        const request = await axios.get();
+        console.log(request);
+        setCountriesData(request.data);
 
     }
 
@@ -28,9 +28,9 @@ const Home = (props) => {
         <div className="container">
             <div className="row">
                     {
-                        parsedData.map((ele) =>{
-                            return <div className="col-md-3" key={ele.name}>
-                                <CountryCard />
+                        countriesData.map((ele) =>{
+                            return <div className="col-auto my-3 mx-4" key={ele.name} style={{width: '20%'}}>
+                                <CountryCard flag={ele.flags.svg} title={ele.name} population={ele.population} region={ele.region} capital={ele.capital} mode={props.mode} />
                             </div>
                         })
                     }
